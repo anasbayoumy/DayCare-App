b24c2dce08d4ff4d57ddaf4305a787342038d762
Anas Bayoumy <167243832+anasbayoumy@users.noreply.github.com>
2025-04-19 19:55:02 +0200

    heh

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:daycarefirst/features/auth/presentation/pages/Login_page.dart';
 import 'package:daycarefirst/features/auth/presentation/pages/signup_page.dart';
+import 'package:daycarefirst/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase here if needed
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
